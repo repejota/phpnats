9f0dc7c19cd6032989b78fb12a20ce73ab96bcf3
Raül Pérez <raul.perez@r3labs.io>
2015-06-26 19:24:20 +0200

    Remove var dumps

--- examples/connect.php
+++ examples/connect.php
@@ -7,13 +7,5 @@ const PORT = 4222;
 echo "Server: nats://" . HOST . ":" . PORT . PHP_EOL;
 echo "Connecting ..." . PHP_EOL;
 $c = new Nats\Connection();
-
-var_dump($c);
-
 $c->connect();
-
-var_dump($c);
-
 $c->close();
-
-var_dump($c);
