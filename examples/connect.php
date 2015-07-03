c34c4eb346bcb7119c1dcf4be7024b341056c86e
Raül Pérez <raul.perez@r3labs.io>
2015-07-03 02:09:46 +0200

    Added auth support

--- examples/connect.php
+++ examples/connect.php
@@ -12,12 +12,6 @@
  */
 require_once "../vendor/autoload.php";
 
-const HOST = "localhost";
-const PORT = 4222;
-
-echo "Server: nats://" . HOST . ":" . PORT . PHP_EOL;
 $c = new Nats\Connection();
-echo "Connecting ..." . PHP_EOL;
 $c->connect();
-echo "Disconnecting ..." . PHP_EOL;
 $c->close();
