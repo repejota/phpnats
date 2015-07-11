d43177e141226fe34ba7bd77bd7b040a0d56b044
Raül Pérez <raul.perez@r3labs.io>
2015-07-11 16:58:13 +0200

    lint

--- tests/Unit/ConnectionTest.php
+++ tests/Unit/ConnectionTest.php
@@ -58,7 +58,7 @@ class ConnectionTest extends \PHPUnit_Framework_TestCase
     {
         $options = new ConnectionOptions();
         if (!self::$isGnatsd) {
-            time_nanosleep(2,0);
+            time_nanosleep(2, 0);
             $options->port = 55555;
         }
         $this->c = new Nats\Connection($options);
