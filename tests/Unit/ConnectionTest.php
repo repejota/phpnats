bfc8c8b3dcbdbd79797ebd218330f5113f9d10e1
jgil <jgil@trovit.com>
2015-07-24 12:53:14 +0200

    Delay for every test.

--- tests/Unit/ConnectionTest.php
+++ tests/Unit/ConnectionTest.php
@@ -61,7 +61,7 @@ class ConnectionTest extends \PHPUnit_Framework_TestCase
     {
         $options = new ConnectionOptions();
         if (!self::$isGnatsd) {
-            time_nanosleep(1, 0);
+            time_nanosleep(2, 0);
             $options->port = 4222;
         }
         $this->c = new Nats\Connection($options);
