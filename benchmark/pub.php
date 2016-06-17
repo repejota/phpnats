11fa432f66714d6a2d43dabff5ec068a2a5c1c96
Raül Pérez <repejota@gmail.com>
2016-06-17 18:26:18 +0200

    Added mini benchmark about published messages

---
+++ benchmark/pub.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$start = microtime(true);
+
+$connectionOptions = new \Nats\ConnectionOptions();
+$connectionOptions->setHost('localhost')->setPort(4222);
+
+$c = new Nats\Connection($connectionOptions);
+$c->connect();
+
+$limit = 1000000;
+for ($i = 1; $i <= $limit; $i++) {
+    $c->publish("foo");
+}
+
+$c->close();
+
+$time_elapsed_secs = microtime(true) - $start;
+
+$speed = $limit/$time_elapsed_secs;
+print "Published " . $limit . " messages in " . $time_elapsed_secs . " seconds" . PHP_EOL;
+print round($speed) . " messages/second" . PHP_EOL;
