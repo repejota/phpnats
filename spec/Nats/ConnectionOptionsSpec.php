077c139c23bc372c94c00198ff3130030014c7d6
Raül Pérez <repejota@gmail.com>
2016-07-27 23:29:03 +0200

    Bump 0.8.0

--- spec/Nats/ConnectionOptionsSpec.php
+++ spec/Nats/ConnectionOptionsSpec.php
@@ -33,7 +33,7 @@ class ConnectionOptionsSpec extends ObjectBehavior
     }
 
     function it_has_default_version_value_as_null() {
-        $this->getVersion()->shouldEqual("0.7.2");
+        $this->getVersion()->shouldEqual("0.8.0");
     }
 
     function it_has_default_verbose_value_as_null() {
