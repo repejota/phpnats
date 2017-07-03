7dd5a4d0dbf33c1e07be1b78355598491f26c140
Raül Pérez <repejota@gmail.com>
2017-07-03 17:06:27 +0200

    lint source code

--- examples/simpleclient/main.php
+++ examples/simpleclient/main.php
@@ -6,25 +6,23 @@ use Nats\Connection as NatsClient;
 $client = new NatsClient();
 $client->connect();
 
-printf("Connected to NATS." . PHP_EOL);
-
-// Publish Subscribe.
+printf('Connected to NATS.'.PHP_EOL);
 
 // Simple Subscriber.
 $client->subscribe(
     'foo',
     function ($message) {
-        printf("Data: %s\r\n" . PHP_EOL, $message->getBody());
+        printf("Data: %s\r\n".PHP_EOL, $message->getBody());
     }
 );
-printf("Subscribed to 'foo' messages." . PHP_EOL);
+printf('Subscribed to "foo" messages.'.PHP_EOL);
 
 // Wait for messages.
-printf("Wait for messages on %s subscriptions" . PHP_EOL, $client->subscriptionsCount());
+printf('Wait for messages on %s subscriptions'.PHP_EOL, $client->subscriptionsCount());
 while (true) {
     $client->wait();
 
-    printf("Reconnecting ..." . PHP_EOL);
+    printf('Reconnecting ...'.PHP_EOL);
     $client->reconnect();
-    printf("Reconnected %s subscriptions" . PHP_EOL, $client->subscriptionsCount());
+    printf('Reconnected %s subscriptions'.PHP_EOL, $client->subscriptionsCount());
 }
