9a1fd97b26d0b094bdcec40d57da381ed35b17d5
oxidmod <melko.v@hiveon.net>
2022-10-13 16:58:39 +0300

    bugfix: fix reading

--- src/Nats/Connection.php
+++ src/Nats/Connection.php
@@ -358,7 +358,8 @@ class Connection
             $chunkSize     = $this->chunkSize;
             $line          = null;
             $receivedBytes = 0;
-            while ($receivedBytes < $len) {
+            $isEof = feof($this->streamSocket);
+            while ($receivedBytes < $len && !$isEof) {
                 $bytesLeft = ($len - $receivedBytes);
                 if ($bytesLeft < $this->chunkSize) {
                     $chunkSize = $bytesLeft;
@@ -367,6 +368,16 @@ class Connection
                 $readChunk      = fread($this->streamSocket, $chunkSize);
                 $receivedBytes += strlen($readChunk);
                 $line          .= $readChunk;
+
+                if (!$readChunk) {
+                    $isEof = feof($this->streamSocket);
+                }
+            }
+
+            if ($receivedBytes < $len) {
+                throw new \Exception(
+                    sprintf('Can\'t read message from socket. Got %d bytes from %d.', $receivedBytes, $len)
+                );
             }
         } else {
             $line = fgets($this->streamSocket);
@@ -401,6 +412,10 @@ class Connection
     private function handleMSG($line)
     {
         $parts   = explode(' ', $line);
+        if (count($parts) < 4) {
+            throw new \Exception(sprintf('Invalid MSG data received: %s', $line));
+        }
+
         $subject = $parts[1];
         $length  = trim($parts[3]);
         $sid     = $parts[2];
